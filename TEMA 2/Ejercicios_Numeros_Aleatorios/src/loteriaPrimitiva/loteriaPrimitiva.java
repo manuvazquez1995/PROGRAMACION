179da6813e6be6a13e98ebe42a3afc06787b8cfc
ATEIS\a13manuelvs <[manuvazquez1995@gmail.com]>
2022-12-16 20:57:20 +0100

    Commit_16/12/2022_CLASE

--- TEMA 2/Ejercicios_Numeros_Aleatorios/src/loteriaPrimitiva/loteriaPrimitiva.java
+++ TEMA 2/Ejercicios_Numeros_Aleatorios/src/loteriaPrimitiva/loteriaPrimitiva.java
@@ -13,11 +13,57 @@ public class loteriaPrimitiva {
         int[] reinGan = new int[2];
 
         //Generamos los números de la combinación ganadora.
+        crearComb(combGan);
+        crearRein(reinGan);
 
+        // Mostramos la combinación ganadora.
+        for(int i=0; i<combGan.length; i++){
+            System.out.print(combGan[i]+" ");
+        }
+
+        System.out.println("");
+
+        for(int i=0; i<reinGan.length; i++){
+            System.out.print(reinGan[i]+" ");
+        }
 
 
 
     }
+
+
+    // Método para crear el número premiado.
+    public static int[] crearComb(int[] combGan){
+
+        //Generamos los números al array que recibe.
+        for(int i=0; i< combGan.length; i++){
+            // Variable para comprobar que el número generado no es igual a algún número introducido.
+            boolean ok = false;
+            // Con el siguiente while comprobamos y creamos que el número no se repite con los anteriores.
+            while(!ok){
+                int nAleatorio = (int) (Math.random()*49)+1;
+                for(int x=0; x < combGan.length; x++){
+                    if(combGan[i]!=nAleatorio){   // TODO: esta condición está mal, no comprueba repetidos.
+                        combGan[i] = nAleatorio;
+                        ok = true;
+                    }
+                }
+            }
+        }
+        return combGan;
+    }
+
+    // Método para crear el reintegro.
+    public static int[] crearRein(int[] reinGan){
+        for(int i=0; i<reinGan.length; i++){
+            reinGan[i] = (int) (Math.random()*9)+1;
+        }
+        return reinGan;
+    }
+
+
+
+
 
 
 
