20a82ce4f668f55230c2b460b7c7acd618f81669
ATEIS\a13manuelvs <[manuvazquez1995@gmail.com]>
2023-01-13 20:56:36 +0100

    Commit_13/01/2023_CLASE

--- TEMA 3/Ejercicios Arrays-Cadenas/src/Arrays_Unidimensionales/Ejer_7.java
+++ TEMA 3/Ejercicios Arrays-Cadenas/src/Arrays_Unidimensionales/Ejer_7.java
@@ -9,12 +9,18 @@ public class Ejer_7 {
     y lo muestre por pantalla. */
     public static void main(String[] args) throws IOException{
 
+        //Creamos variables p y q y les asignamos valores.
+        int p = pedirP();
+        int q = pedirQ(p);
 
+        //Creamos array, llamamos al método que cuenta los valores existentes entre p y q y se lo asigna.
+        int[] listaNums = new int[cantidadNums(p,q)];
 
+        //Asigna valores al array con el siguiente método.
+        asignarValores(listaNums, p, q);
 
-
-
-
+        //Mostramos los valores del array con el siguiente método.
+        mostrarValores(listaNums);
 
     }
 
@@ -27,7 +33,7 @@ public class Ejer_7 {
 
     //El método pedirQ pide el valor Q y comprueba que sea mayor que P.
     public static int pedirQ(int p) throws IOException{
-        System.out.println("- Introduce M (nº a asignar): ");
+        System.out.println("- Introduce Q: ");
         int q = bufferReader.leerInt();
         while(q < p){
             System.out.println("Valor Q no puede ser menor que P.\n" +
@@ -37,6 +43,31 @@ public class Ejer_7 {
         return q;
     }
 
+    //Método que comprueba la cantidad de números que hay entre P y Q.
+    public static int cantidadNums(int p, int q){
+        int c = 0;
+        for(int i = p; i <= q; i++){ //Cuenta los números que hay entre los 2 valores.
+            c++;
+        }
+        return c;
+    }
+
+    //Método que asigna los valores en el array.
+    public static void asignarValores(int[] listaNums, int p, int q) {
+        int y = 0; //El valor que se guarda aquí es la posición del array al que tenemos que acceder, se autoincrementa.
+        for(int i = p; i <= q; i++){
+            listaNums[y] = i;
+            y++;
+        }
+    }
+
+    //Método que muestra los valores del array.
+    public static void mostrarValores(int[] listaNums){
+        System.out.println("- Valores entre P y Q: ");
+        for(int n : listaNums){
+            System.out.print("|"+n+"|");
+        }
+    }
 
 
 }
---
+++ TEMA 3/Ejercicios Arrays-Cadenas/src/Arrays_Unidimensionales/Ejer_9.java
@@ -0,0 +1,57 @@
+package Arrays_Unidimensionales;
+import metodosReutilizables.*;
+import java.io.IOException;
+
+public class Ejer_9 {
+
+    /* 9. Crea un programa que cree un array de enteros de tamaño 100 y lo rellene con valores enteros aleatorios
+    entre 1 y 10. Luego pedirá un valor N y mostrará en qué posiciones del array aparece N. */
+    public static void main(String[] args) throws IOException {
+
+        //Creamos el array.
+        int[] listaNums = new int[100];
+
+        //Rellenamos el array con un método.
+        rellenarArray(listaNums);
+
+        //Mostramos el array con un método.
+        mostrarValores(listaNums);
+
+        //Mostramos en que posiciones del array está un número pedido por teclado.
+        buscarNumero(listaNums);
+
+    }
+
+    //Método que rellena el array con números aleatorios.
+    public static void rellenarArray(int[] listaNums){
+        for(int i=0; i<listaNums.length; i++){
+            listaNums[i] = (int) (Math.random()*11);
+        }
+    }
+
+    //Método que muestra los valores del array.
+    public static void mostrarValores(int[] listaNums){
+        for(int i=0; i<listaNums.length; i++){
+            switch (i){ //Para que lo muestre en 2 filas y no en 1 porque es incómodo verlo.
+                case 49 -> System.out.println();
+                default -> System.out.print("|"+listaNums[i]);
+            }
+        }
+        System.out.println();
+        System.out.println("================================================================");
+    }
+
+    //Método que busca en qué posiciones del array está un número pedido por teclado (N).
+    public static void buscarNumero(int[] listaNums) throws IOException {
+        System.out.println("- Introduce un número a buscar: ");
+        int n = bufferReader.leerInt();
+        System.out.println("El número "+n+" está en las siguientes posiciones: ");
+        for(int i = 0; i<listaNums.length; i++){
+            if(n == listaNums[i]){
+                System.out.print("|"+i);
+            }
+        }
+    }
+
+
+}
