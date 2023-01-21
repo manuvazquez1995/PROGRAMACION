49ce542db9917cddbe830cd021441bb2c3ba10ae
manu <manuvazquez1995@gmail.com>
2023-01-21 11:59:41 +0100

    Commit_21/01/2023_CASA

--- TEMA 3/Ejercicios Arrays-Cadenas/src/Arrays_Bidimensionales/Ejer_2.java
+++ TEMA 3/Ejercicios Arrays-Cadenas/src/Arrays_Bidimensionales/Ejer_2.java
@@ -15,31 +15,47 @@ public class Ejer_2 {
         int[][] matriz = new int[4][5];
         // Para guardar la suma de cada fila.
         int[] sumasFilas = new int[4];
-        //Para guardar la suma de cada columna.
+        // Para guardar la suma de cada columna.
         int[] sumasCol = new int[5];
+        // Para guardar la suma total de las filas más de las columnas.
+        int sumaTotal = 0;
 
-        // Pedimos los números y los vamos metiendo en la matriz.
+        // Pedimos los números y los vamos metiendo en la matriz. También vamos sumando las filas y las columnas.
         for(int i=0; i<matriz.length; i++){  //Para recorrer las filas.
             for(int j=0; j<matriz[i].length; j++){       // Para recorrer las columnas.
                 System.out.println("- Introducir número: ");
                 int num = bufferReader.leerInt();
                 matriz[i][j] = num;
-                sumasFilas[i] = sumasFilas[i] + num; // Suma los valores de una columna.
+                sumasFilas[i] = sumasFilas[i] + num; // Suma los valores de una fila.
+                sumasCol[j] = sumasCol[j] + num;  // Suma los valores de una columna.
             }
         }
 
+        // Sumamos el total de las filas con el total de las columnas.
+        for (int n : sumasFilas) {   // Para hacer el total de las filas, guardándolo directamente en sumaTotal.
+            sumaTotal = sumaTotal + n;
+        }
+        for(int n : sumasCol){
+            sumaTotal = sumaTotal + n; // Para hacer el total de las columnas, guardándolo directamente en sumaTotal.
+        }
+
+
         // Mostramos el contenido del array en forma de tabla.
         System.out.println();
-        for(int i=0; i<matriz.length; i++){
+        for(int i=0; i<matriz.length; i++){   // Printeamos las filas.
             System.out.print("Fila "+i+"||\t");
             for(int j=0; j<matriz[i].length; j++) {
                 System.out.print(matriz[i][j]+"\t   ");
 
             }
-            System.out.print("||Suma Fila: "+sumasFilas[i]);
+            System.out.print("||Suma Fila: "+sumasFilas[i]);   // Printeamos la suma de cada fila al final de las filas.
             System.out.println();
         }
-
+        System.out.print("Sum cols: \t");
+        for (int j : sumasCol) {    // Printeamos la suma de cada columnas en una sola fila, coincidiendolas con las su columna.
+            System.out.print(j + "\t   ");
+        }
+        System.err.print("||Suma total: "+sumaTotal);
 
     }
 
